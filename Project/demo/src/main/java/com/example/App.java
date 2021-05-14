245d95ce9fc260d07942e1229a946d4f35e14692
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-14 12:21:19 +0200

    app : created json gen

--- Project/demo/src/main/java/com/example/App.java
+++ Project/demo/src/main/java/com/example/App.java
@@ -3,9 +3,13 @@ package com.example;
 import com.example.util.*;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -54,14 +58,47 @@ public class App
 
              /* Generar dos XMLs de salida, uno con todas las
             operaciones de tipo venta y otro con todas las operaciones de tipo reparación */
-            xmlUtil.exportByOperacionType(documents, ventasDocument, reparacionesDocument);
+            //xmlUtil.exportByOperacionType(documents, ventasDocument, reparacionesDocument);
             
-            
+            /* Pintar JSON con todas las operaciones y sus respectivos precios de todos los concesionarios */
 
-            
-            
-        } catch (Exception e) {
+            /* Creación de xpath */
+            XPath xPath = XPathFactory.newInstance().newXPath();
+
+            /* Creación de array operaciones */
+            JSONArray operaciones = new JSONArray();
+
+            /* Creación de JSON root y adición de operaciones*/
+            JSONObject json = new JSONObject();
+            json.put("operaciones", operaciones);
+
+            /* Creación de JSON precio */
+            JSONObject precio = new JSONObject();
+
+            /* Recorre los nodos operacion */
+            for (Document document : documents) {
+                NodeList nodosPrecio = (NodeList) xPath.compile("//precio").evaluate(document, XPathConstants.NODESET);
+                for(int i=0; i < nodosPrecio.getLength(); i++){
+                    Node nodoPrecio = nodosPrecio.item(i);
+                    String precioText = nodoPrecio.getTextContent();
+                    precio.put("precio", precioText);
+                    operaciones.put(precio);
+                }
+            }
+
+            /* Escribir y pintar JSON */
+            try{
+                FileWriter jsonFile = new FileWriter("exports/operaciones.json");
+                jsonFile.write(json.toString());
+                jsonFile.flush();
+                jsonFile.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+                System.out.println(json.toString());
+        
+            } catch (Exception e) {
             e.printStackTrace();
-        }
+            }
     }
 }
