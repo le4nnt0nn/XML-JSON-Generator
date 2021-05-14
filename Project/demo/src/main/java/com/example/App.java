59643ec0c829ce4fabacdec3444f1027464165a6
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-14 10:43:09 +0200

    app : imports and created doc list

--- Project/demo/src/main/java/com/example/App.java
+++ Project/demo/src/main/java/com/example/App.java
@@ -1,5 +1,22 @@
 package com.example;
 
+import com.example.util.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 /**
  * Hello world!
  *
@@ -8,6 +25,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        try {
+            /* Recolección de ficheros xml en Lista */
+            File file = new File("./src/xmls");
+            File[] xmls = file.listFiles();
+
+            List<Document> documents = new ArrayList<Document>();
+            for(int i=0; i<xmls.length; i++){
+                File xml = xmls[i];
+                Document document = xmlUtil.getXMLDocument(xml.getAbsolutePath());
+                documents.add(document);
+            }
+
+            XPath xPath = XPathFactory.newInstance().newXPath();
+            
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
