57c62cbed1122740ab9abce2b818cf5727f51e71
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-14 11:09:01 +0200

    xmlUtil : created method exportAlarmas

--- Project/demo/src/main/java/com/example/util/xmlUtil.java
+++ Project/demo/src/main/java/com/example/util/xmlUtil.java
@@ -1,6 +1,7 @@
 package com.example.util;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -56,6 +57,45 @@ public class xmlUtil {
         }
 
         return doc;
+    }
+
+    public static void exportConcesionarioAlarma(List<Document> documents) throws ParserConfigurationException, XPathExpressionException{
+        /* Creación de xpath */
+        XPath xPath = XPathFactory.newInstance().newXPath();
+            
+        /* Creación de Lista alarmas (recoge todos los docs < 5 operaciones) */
+
+        /**
+        * El único concesionario con más de 5 operaciones es QueMazda (concesionario2.xml) 
+        *
+        */
+        List<Document> alarmas = new ArrayList<Document>();
+        /* Recoger todos los documentos con menos de 5 operaciones */
+        for (Document document : documents) {
+            NodeList nodosOperacion = (NodeList) xPath.compile("//operacion").evaluate(document, XPathConstants.NODESET);
+            if(nodosOperacion.getLength() < 5){
+                alarmas.add(document);
+            }
+        }
+
+        /* Creación de finalDocument */
+        DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+        Document finalDocument = documentBuilder.newDocument();
+        /* Root implementado en el documentoFinal */
+        Element root = finalDocument.createElement("root");
+        finalDocument.appendChild(root);
+
+        /* Recorro los documentos de alarmas, devuelven sus elementos, los importo y 
+        los paso a un nodo final (finalConcesionario) */
+        for (Document document : alarmas) {
+            Element concesionario = document.getDocumentElement();
+            Node finalConcesionario = finalDocument.importNode(concesionario, true);
+            root.appendChild(finalConcesionario);
+        }
+        /* Escribe y exporta el contenido de finalDocument con el sufijo alarma */
+        xmlUtil.writeDocument(finalDocument, "alarma");
+
     }
     
 }
